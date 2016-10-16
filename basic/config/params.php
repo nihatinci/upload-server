7df974a1dbcdb3104aa4bf12352aed1b12a936c9
ninci <ninci@internetbrands.com>
2016-10-16 13:48:13 -0700

    removing unnecessary items

--- basic/config/params.php
+++ basic/config/params.php
@@ -2,17 +2,6 @@
 
 return [
   'adminEmail' => 'admin@example.com',
-  'shipstation'=> array(
-    'url' => 'https://ssapi.shipstation.com/',
-    'apiKey'=>'7d3c55e960284d4d94f443782db38917',
-    'apiSecret'=>'bed61da5ef5e410d99a340b1d21581fd',
-    'storeId' => 51629
-  ),
-  'smtp' => [
-    'username' => 'personalizedtrendsla@gmail.com',
-    'password' => 'f3fRuWA=$Afr'
-    //'password' => 'f3fkjkjhlk'
-  ]
 
 ];
 
